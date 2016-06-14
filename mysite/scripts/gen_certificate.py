577d2b510b249049773ac8e0fa7768d6d5028319
Pocketpc <pongpongcn@gmail.com>
2016-06-14 14:43:48 +0000

    clean

--- mysite/scripts/gen_certificate.py
+++ mysite/scripts/gen_certificate.py
@@ -6,19 +6,6 @@ from scipy.stats import norm
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
 
-def runx():
-    xx=np.arange(0,10,0.01)
-    yy=xx*np.exp(-xx)
-
-    path = Path(np.array([xx,yy]).transpose())
-    patch = PathPatch(path, facecolor='none')
-    plt.gca().add_patch(patch)
-
-    im = plt.imshow(np.vstack((xx, xx)),  cmap=plt.cm.gist_rainbow,interpolation="bicubic",
-                    origin='lower',extent=[0,10,-0.0,0.40],aspect="auto", clip_path=patch, clip_on=True)
-
-    plt.show()
-
 def run():
     studentEvaluation = StudentEvaluation.objects.all()[0]
     _gen_certificates([studentEvaluation,], True)
@@ -32,11 +19,6 @@ def run():
     ylimmax = max(y) * 1.2
     
     plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-    #plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-    
-    #rainbow_fill(x,y)
-    
-    #plt.figure(figsize=(1,1))
     
     pathPoints = np.array([x,y]).transpose()
     pathPoints = np.concatenate((pathPoints,[(900,0), (0,0)]))
@@ -58,9 +40,6 @@ def run():
     plt.text(500, ylimmax, 'SELF',
         verticalalignment='bottom', horizontalalignment='center', fontsize=15)
         
-    '''plt.axis([0, 6, 0, 20])'''
-    #plt.ylim(0,ylimmax * 2)
-    #plt.legend()
     plt.axis('off')
     
     plt.text(100, -1*ylimmax*0.01, 'BELOW STANDARD',
@@ -72,23 +51,8 @@ def run():
     plt.text(800, -1*ylimmax*0.01, 'OPTIMAL',
         verticalalignment='top', horizontalalignment='center', fontsize=15)
     
-    #plt.show()
-    plt.savefig('demo', transparent=True)
-
-def rect(x,y,w,h,c):
-    ax = plt.gca()
-    polygon = plt.Rectangle((x,y),w,h,color=c)
-    ax.add_patch(polygon)
-
-def rainbow_fill(X,Y, cmap=plt.get_cmap("jet")):
-    plt.plot(X,Y,lw=0)  # Plot so the axes scale correctly
-
-    dx = X[1]-X[0]
-    N  = float(X.size)
-
-    for n, (x,y) in enumerate(zip(X,Y)):
-        color = cmap(n/N)
-        rect(x,0,dx,y,color)
+    plt.show()
+    #plt.savefig('demo', transparent=True)
     
 def _gen_certificates(studentEvaluations, isAdmin):
     '''
